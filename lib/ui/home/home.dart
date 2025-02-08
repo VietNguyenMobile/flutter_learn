73bc90f80387f5ea0187c36d321089844c0de204
Viet Nguyen <>
2025-02-08 20:51:38 +0700

    Flutter 6: Tạo giao diện chi tiết cho từng bài hát trong danh sách

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:music_app_demo/data/model/song.dart';
 
 import 'package:music_app_demo/ui/discovery/discovery.dart';
+import 'package:music_app_demo/ui/home/viewmodel.dart';
 import 'package:music_app_demo/ui/user/account.dart';
 import 'package:music_app_demo/ui/settings/settings.dart';
 
@@ -30,7 +32,7 @@ class MusicHomePage extends StatefulWidget {
 
 class _MusicHomePageState extends State<MusicHomePage> {
   final List<Widget> _pageTabs = [
-    const HomePageTab(),
+    const HomeTab(),
     const DiscoveryTab(),
     const AccountTab(),
     const SettingsTab(),
@@ -62,15 +64,125 @@ class _MusicHomePageState extends State<MusicHomePage> {
   }
 }
 
-class HomePageTab extends StatelessWidget {
-  const HomePageTab({super.key});
+class HomeTab extends StatelessWidget {
+  const HomeTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home Tab"),
-      )
+    return HomeTabPage();
+  }
+}
+
+class HomeTabPage extends StatefulWidget {
+  const HomeTabPage({super.key});
+
+  @override
+  State<HomeTabPage> createState() => _HomeTabPageState();
+}
+
+class _HomeTabPageState extends State<HomeTabPage> {
+  List<Song> songs = [];
+  late MusicAppViewModel _viewModel;
+
+  @override
+  void initState() {
+    _viewModel = MusicAppViewModel();
+    _viewModel.loadSongs();
+    observeData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: getBody(),
     );
+  }
+
+  @override
+  void dispose() {
+    _viewModel.songStream.close();
+    super.dispose();
+  }
+
+  Widget getBody() {
+    bool showLoading = songs.isEmpty;
+    if (showLoading) {
+      return getProgressBar();
+    } else {
+      return getListView();
+    }
+  }
+
+  Widget getProgressBar() {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  ListView getListView() {
+    return ListView.separated(
+      itemBuilder: (context, position) {
+        return getRow(position);
+      },
+      separatorBuilder: (context, index) {
+        return const Divider(
+          color: Colors.grey,
+          thickness: 1,
+          indent: 24,
+          endIndent: 24,
+        );
+      },
+      itemCount: songs.length,
+      shrinkWrap: true,
+    );
+  }
+
+  Widget getRow(int position) {
+    return _SongItemSection(parent: this, song: songs[position]);
+  }
+
+  void observeData() {
+    _viewModel.songStream.stream.listen((event) {
+      setState(() {
+        songs = event;
+      });
+    });
+  }
+}
+
+class _SongItemSection extends StatelessWidget {
+  const _SongItemSection({
+    required this.parent,
+    required this.song,
+  });
+
+  final _HomeTabPageState parent;
+  final Song song;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: const EdgeInsets.only(
+        left: 24,
+        right: 8
+      ),
+        leading: ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: FadeInImage.assetNetwork(
+              placeholder: 'assets/itune.jpeg',
+              image: song.image,
+              width: 48,
+              height: 48,
+              imageErrorBuilder: (context, error, stackTrace) {
+                return Image.asset('assets/itune.jpeg', width: 48, height: 48);
+              },
+            )),
+        title: Text(song.title),
+        subtitle: Text(song.artist),
+        trailing: IconButton(
+          icon: const Icon(Icons.more_horiz),
+          onPressed: () {},
+        ));
   }
 }
